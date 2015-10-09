8ad99185cc4bee8e16d5398c332278eb9b790c12
unknown <SLL120@CILABS-119A-18.scilsnet.rutgers.edu>
2015-10-08 22:27:36 -0400

    Done

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+	static public FollowCam S;
+	public float easing = 0.05f;
+	public Vector2 minXY;
+	public bool __________________________;
+	public GameObject poi;
+	public float camZ;
+
+	void Awake (){
+		S = this;
+		camZ = this.transform.position.z;
+	}
+	void FixedUpdate(){
+		Vector3 destination;
+		if (poi == null) {
+			destination = Vector3.zero;
+		} else {
+			destination = poi.transform.position;
+			if (poi.tag == "Projectile") {
+				if (poi.rigidbody.IsSleeping ()) {
+					poi = null;
+					return;
+				}
+			}
+		}
+
+		//limit x and y
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
+		destination = Vector3.Lerp (transform.position, destination, easing);
+		destination.z = camZ;
+		transform.position = destination;
+		this.camera.orthographicSize = destination.y + 10;
+	}
+
+
+	
+}
+
---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+	static public Slingshot S;
+	public GameObject prefabProjectile;
+	public float velocityMult = 4f;
+	public bool _________________________;
+
+	public GameObject launchPoint;
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
+
+	void Update(){
+		if (!aimingMode)
+			return;
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+		Vector3 mouseDelta = mousePos3D - launchPos;
+		float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+		if (mouseDelta.magnitude > maxMagnitude) {
+			mouseDelta.Normalize ();
+			mouseDelta *= maxMagnitude;
+		}
+		Vector3 projPos = launchPos + mouseDelta;
+		projectile.transform.position = projPos;
+
+		if (Input.GetMouseButtonUp (0)) {
+			aimingMode = false;
+			projectile.rigidbody.isKinematic = false;
+			projectile.rigidbody.velocity = -mouseDelta * velocityMult;
+			FollowCam.S.poi = projectile;
+			projectile = null;
+			MissionDemolition.ShotFired();
+		}
+
+	}
+	void Awake(){
+		S = this;
+		Transform launchPointTrans = transform.FindChild("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+		launchPos = launchPointTrans.position;
+	}
+
+	void OnMouseEnter(){
+		//print ("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive (true);
+	}
+	void OnMouseExit(){
+		//print ("Slingshot:OnMouseExit()");
+		launchPoint.SetActive (false);
+	}
+	void OnMouseDown(){
+		aimingMode = true;
+		projectile = Instantiate (prefabProjectile) as GameObject;
+		projectile.transform.position = launchPos;
+		projectile.rigidbody.isKinematic = true;
+	}
+}
